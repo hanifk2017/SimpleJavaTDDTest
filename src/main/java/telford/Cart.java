210d9472b6f22a6c053b5bb7f492bb654242388d
hanif <hanifk@yahoo.com>
2017-01-18 14:07:09 +0000

    Added discount functionality

--- src/main/java/telford/Cart.java
+++ src/main/java/telford/Cart.java
@@ -1,11 +1,19 @@
 package telford;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * Created by hanif on 18/01/17.
  */
 public class Cart {
+
+    private static final String APPLE_OFFER = "AppleOffer";
+    private static final String ORANGE_OFFER = "OrangeOffer";
+
+    private static final String ORANGE = "Orange";
+
 
     private ProductCatalog productCatalog = new ProductCatalog();
 
@@ -21,11 +29,33 @@ public class Cart {
         for(String productId:productIds){
 
             Double product = productCatalog.getProduct(productId);
+
             if (product == null){
                 throw new IllegalArgumentException("Unknown Product in List");
             }
 
             total += product;
+        }
+
+        return total;
+    }
+
+    public Double calculateTotal(List<String> productIds, String offerCode) {
+
+
+        if (offerCode == null || !(offerCode.equals(APPLE_OFFER) || offerCode.equals(ORANGE_OFFER))){
+            throw new IllegalArgumentException("Unknown Discount Code");
+        }
+
+        double total = calculateTotal(productIds);
+
+        if (offerCode.equals(ORANGE_OFFER)) {
+
+            int noOfOranges = Collections.frequency(productIds,ORANGE);
+
+            double discount = (noOfOranges / 3) * productCatalog.getProduct(ORANGE);
+
+            total -= discount;
         }
 
         return total;
--- src/test/java/telford/CartTest.java
+++ src/test/java/telford/CartTest.java
@@ -15,6 +15,11 @@ public class CartTest {
     private static final String ORANGE = "Orange";
     private static final String BANANAS = "Bananas";
 
+    private static final String APPLE_OFFER = "AppleOffer";
+    private static final String ORANGE_OFFER = "OrangeOffer";
+    private static final String INVALID_OFFER = "InvalidOffer";
+
+
     private Cart cart;
 
     @Before
@@ -51,6 +56,66 @@ public class CartTest {
     public void testTotalWithOneAppleAndTwoOrangesProductsToCart(){
 
         Assert.assertEquals(Double.valueOf(1.10),cart.calculateTotal(Arrays.asList(APPLE,ORANGE,ORANGE)));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testTotalWithNullDiscountCodeToCart(){
+
+        cart.calculateTotal(Arrays.asList(APPLE,ORANGE,ORANGE),null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testTotalWithInvalidDiscountCodeToCart(){
+
+       cart.calculateTotal(Arrays.asList(APPLE,ORANGE,ORANGE),INVALID_OFFER);
+    }
+
+    @Test
+    public void testApplyAppleDiscountOfferOnOneApple(){
+
+        Assert.assertEquals(Double.valueOf(0.60),cart.calculateTotal(Arrays.asList(APPLE),APPLE_OFFER));
+    }
+
+    @Test
+    public void testApplyAppleDiscountOfferOnTwoApples(){
+
+        Assert.assertEquals(Double.valueOf(1.20),cart.calculateTotal(Arrays.asList(APPLE,APPLE),APPLE_OFFER));
+    }
+
+    @Test
+    public void testApplyOrangeDiscountOfferOnOneOrange(){
+
+        Assert.assertEquals(Double.valueOf(0.25),cart.calculateTotal(Arrays.asList(ORANGE),ORANGE_OFFER));
+    }
+
+    @Test
+    public void testApplyOrangeDiscountOfferOnTwoOranges(){
+
+        Assert.assertEquals(Double.valueOf(0.50),cart.calculateTotal(Arrays.asList(ORANGE,ORANGE),ORANGE_OFFER));
+    }
+
+    @Test
+    public void testApplyOrangeDiscountOfferOnThreeOranges(){
+
+        Assert.assertEquals(Double.valueOf(0.50),cart.calculateTotal(Arrays.asList(ORANGE,ORANGE,ORANGE),ORANGE_OFFER));
+    }
+
+    @Test
+    public void testApplyOrangeDiscountOfferOnFourOranges(){
+
+        Assert.assertEquals(Double.valueOf(0.75),cart.calculateTotal(Arrays.asList(ORANGE,ORANGE,ORANGE,ORANGE),ORANGE_OFFER));
+    }
+
+    @Test
+    public void testApplyOrangeDiscountOfferOnFiveOranges(){
+
+        Assert.assertEquals(Double.valueOf(1.00),cart.calculateTotal(Arrays.asList(ORANGE,ORANGE,ORANGE,ORANGE,ORANGE),ORANGE_OFFER));
+    }
+
+    @Test
+    public void testApplyOrangeDiscountOfferOnSixOranges(){
+
+        Assert.assertEquals(Double.valueOf(1.00),cart.calculateTotal(Arrays.asList(ORANGE,ORANGE,ORANGE,ORANGE,ORANGE,ORANGE),ORANGE_OFFER));
     }
 
 }
